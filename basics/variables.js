fb9b0e59a22d687b0f2e72fb6197df6857c2fcc2
HalimFr23 <halimfr23@gmail.com>
2026-01-21 14:31:35 +0700

    Add variables and console output

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+// user full name
+let name = "Halim Fatur Rohman";
+
+// user age
+let age = 22;
+
+// user status
+let isStudent = true;
+
+console.log("Name: ", name);
+console.log("Age: ", age);
+console.log("Is Student: ", isStudent);
