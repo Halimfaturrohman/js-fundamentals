4f2d34d8caf175d12a73e08a7173bc70fb2c4a11
HalimFr23 <halimfr23@gmail.com>
2026-01-22 23:41:31 +0700

    Add data types example

---
+++ basics/data-type.js
@@ -0,0 +1,10 @@
+let userName = "Halim Fatur Rohman";
+let userAge = "22";// Even if age looks like a number, JavaScript treats it as a string when inside quotes
+
+let isStudent = true;
+
+console.log("Name type:", typeof userName);
+console.log("Age type:", typeof userAge);
+console.log("Student type:", typeof isStudent);
+
+
