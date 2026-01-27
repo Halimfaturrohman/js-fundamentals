02bcb1132725c470afd9ef00a9bf59aa34aac8d9
HalimFr23 <halimfr23@gmail.com>
2026-01-27 23:27:29 +0700

    Add score grading logic

---
+++ basics/score-grading.js
@@ -0,0 +1,11 @@
+let score = 90;
+
+if (score >= 85) {
+    console.log("A");
+} else if ( score >= 70 && score <=84) {
+    console.log("B");
+} else if (score >= 55 && score <= 69) {
+    console.log("C");
+} else {
+    console.log("D");
+}
