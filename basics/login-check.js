0b2719a72106d3b3a88116d85b879cb4383a33cb
HalimFr23 <halimfr23@gmail.com>
2026-01-28 16:50:29 +0700

    add validation logic

---
+++ basics/login-check.js
@@ -0,0 +1,10 @@
+let userName = "admin";
+let passWord = "12345";
+
+if (userName !== "admin") {
+    console.log("User Not Found");
+} else if (passWord !== "12345") {
+    console.log("Wrong Password");
+} else {
+    console.log("Login Success");
+}
